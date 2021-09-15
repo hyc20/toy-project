391573e5eab3ce6a4555d1603a804daa6d48b201
hyc20 <hyc20@naver.com>
2021-09-15 19:55:00 +0900

    [홍용천] dev 브랜치 테스트 종료

--- src/test/http/GitTest.java
+++ src/test/http/GitTest.java
@@ -3,6 +3,6 @@ package test.http;
 public class GitTest {
 	
 	private static final String DESC ="local Git Test용 클래스입니다";
-	
-	
+	private static final String DEV ="dev 브랜치 사용 시작";
+	private static final String DEV_FIN = "dev 브랜치 종료";
 }
