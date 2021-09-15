c766d410f727490946f36e7bea04acbf7b4d1f3a
hyc20 <hyc20@naver.com>
2021-09-15 20:09:11 +0900

    [홍용천] conflict test

--- src/test/http/GitTest.java
+++ src/test/http/GitTest.java
@@ -2,7 +2,7 @@ package test.http;
 
 public class GitTest {
 	
-	private static final String DESC ="local Git Test용 클래스입니다";
-	private static final String DEV ="dev 브랜치 사용 시작";
-	private static final String DEV_FIN = "dev 브랜치 종료";
+	private static final String DESC ="local Git Test용 클래스입니다[local]";
+	private static final String DEV ="dev 브랜치 사용 시작[local]";
+	private static final String DEV_FIN = "dev 브랜치 종료[local]";
 }
